a5844757fd307d36793547ceacb4b3d710c88ea3
Toshimi Kawata <kawata@shyme.net>
2026-03-09 13:57:02 +0900

    v2.2.57

--- web/src/configs/settings.ts
+++ web/src/configs/settings.ts
@@ -3,5 +3,5 @@ export const TOKEN_EXPIRE_HOURS = 87600 // 10年
 export const PUBLIC_PATH = ''
 export const APP_NAME = 'MYCUTE'
 export const APP_CAPTION = 'Growing hearts of sweet AI kids, only for you.'
-export const LOGO_IMG_SRC = `${PUBLIC_PATH}/mycute-white-256.png`
+export const LOGO_IMG_SRC = `${PUBLIC_PATH}/mycute-wrapped-256.png`
 export const API_BASE_URL = 'http://127.0.0.1:8888'
