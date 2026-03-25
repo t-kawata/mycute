7e4a41318c5baf356d2f739a1a124556c317c2df
Toshimi Kawata <kawata@shyme.net>
2026-03-25 14:16:29 +0900

    v2.3.18

--- src/constants.rs
+++ src/constants.rs
@@ -18,7 +18,7 @@ pub const WINDOW_WIDTH: f64 = 390.0;
 pub const WINDOW_HEIGHT: f64 = 750.0;
 
 /// MYCUTE OS のバージョン番号。
-pub const MYCUTE_VERSION: &str = "v2.3.17";
+pub const MYCUTE_VERSION: &str = "v2.3.18";
 
 /// アプリケーション名（ロックファイル等で使用）
 pub const APP_NAME: &str = "mycute";
