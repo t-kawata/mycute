1d0cbba8c730f06e00fb4b3f4dad62a75545b1d3
shyme01 <shyme01@shyme01noMac-Studio.local>
2026-04-20 14:56:21 +0900

    v2.3.59

--- src/constants.rs
+++ src/constants.rs
@@ -18,7 +18,7 @@ pub const WINDOW_WIDTH: f64 = 390.0;
 pub const WINDOW_HEIGHT: f64 = 750.0;
 
 /// MYCUTE OS のバージョン番号。
-pub const MYCUTE_VERSION: &str = "v2.3.58";
+pub const MYCUTE_VERSION: &str = "v2.3.59";
 
 /// アプリケーション名（ロックファイル等で使用）
 pub const APP_NAME: &str = "mycute";
