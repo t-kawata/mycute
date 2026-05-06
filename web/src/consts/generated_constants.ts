0c898e015c9ca3ab76ccee6fbf8544caf56aee24
Toshimi Kawata <kawata@shyme.net>
2026-05-06 16:34:19 +0900

    v2.4.17

--- web/src/consts/generated_constants.ts
+++ web/src/consts/generated_constants.ts
@@ -6,7 +6,7 @@
 
 export const WINDOW_WIDTH = 390.0;
 export const WINDOW_HEIGHT = 750.0;
-export const MYCUTE_VERSION = "v2.4.16";
+export const MYCUTE_VERSION = "v2.4.17";
 export const APP_NAME = "mycute";
 export const APP_DISPLAY_NAME = "MYCUTE";
 export const APP_SERVER_NAME = "mycute-server";
